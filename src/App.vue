0552d4c7c0051a77a1774df54bbe218c4c4fd300
yravindraswamy <yravindraswamy@gmail.com>
2024-02-17 12:01:43 +0530

    these are changes from features2

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,81 @@
 <template>
-  <form @action="javascript:void(0);" class="form-control">
-    <div >
-      <label for="name" class="form-label">First Name:</label>
-      <input type="text" v-model="firstName" id="name" class="form-text"/>
+  <form>
+  <div>
+    <label>
+      <span>E-mail:</span>
+      <input v-model="email" class="form-control" ref="email">
+<!--      <div v-if="v$.email.$error" :class="{'error':v$.email.$error}" class="error-msg">Please Enter your E-mail{{v$.email.minLength.$params.min}}</div>-->
+      <div ></div>
+    </label>
     </div>
     <div>
-      <label for="lastName" class="form-label">Last Name:</label>
-      <input type="text" v-model="lastName" id="lastName" class="form-text"/>
+      <label>
+        <span>Password:</span>
+        <input v-model="passwords.password" class="form-control">
+<!--        <div v-if="v$.passwords.password.$error" :class="{'error':v$.passwords.password.$error}" class="error-msg">Password must be {{v$.passwords.password.minLength.$params.min}} characters</div>-->
+        <div ></div>
+      </label>
     </div>
     <div>
-      <label for="email" class="form-label">Email :</label>
-      <input type="text" v-model="email" id="email" class="form-text"/>
+      <label>
+        <span>confirm Password: </span>
+        <input v-model="passwords.confirmPassword" class="form-control">
+<!--        <div v-if="v$.passwords.confirmPassword.$error" :class="{'error':v$.passwords.confirmPassword.$error}" class="error-msg">Please Enter your password</div>-->
+        <div ></div>
+      </label>
     </div>
-    <div>
-      <vue-tel-input v-model="phone" :dropdownOptions="dropdownOptions" :inputOptions="inputOptions"></vue-tel-input>
-    </div>
+    <button type="submit" class="btn btn-primary" @click.prevent.stop="submitForm">Submit</button>
+    <button class="btn btn-info" @click.prevent.stop="v$.$reset()">Reset</button>
   </form>
 </template>
 
 <script>
 import { useVuelidate } from "@vuelidate/core";
-import { required, email } from "@vuelidate/validators";
+import { required,/*email,minLength*/} from "@vuelidate/validators";
 
 export default {
   name: "App",
-  setup() {
-    return { v$: useVuelidate() };
-  },
   data() {
     return {
-      firstName: "",
-      lastName: "",
-      contact: {
-        email: "",
+      v$: useVuelidate(),
+      email: null,
+      passwords:{
+        password:null,
+        confirmPassword:null
       },
-      phone:null,
-      dropdownOptions:{
-        showDialCodeInSelection:true,
-        showFlags:true,
-        showSearchBox:true
-      },
-      inputOptions:{
-        autofocus: true,
-        showDailCode:true,
-        placeholder:'Enter Mobile Number'
+      passwordMustbe: false
+    };
+  },
+  methods:{
+    async submitForm(){
+      const isFormValid = await this.v$.$validate()
+      console.log(this.v$)
+      if(!isFormValid) return
+    }
+  },
+  validations(){
+    const localRules={
+     email:{required}
+    }
+    if(!this.passwordMustbe){
+      const passwords={
+        password:{required},
+        confirmPassword: {required}
       }
-    };
-  },
-  validations() {
-    return {
-      firstName: { required },
-      lastName: { required },
-      const: {
-        email: { require, email },
-      },
-    };
-  },
+      localRules['password'] = passwords
+    }
+    return{
+      // email:{required,email,minLength:minLength(10)},
+      // passwords:{
+      //   password:{
+      //     required,
+      //     minLength:minLength(10)
+      //   },
+      //   confirmPassword:{required}
+      // }
+      localRules
+    }
+  }
 };
 </script>
 
@@ -67,5 +87,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.error-msg{
+  color:red;
+  font-size: 14px;
+  display:none;
+}
+.error{
+  color:red;
+  font-size: 14px;
+  display: block;
 }
 </style>
